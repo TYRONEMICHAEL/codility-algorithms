acd42f0203bf7c530c3ba6e20ea23192ce07c785
Tyrone Michael Avnit <tyronemichael@gmail.com>
2016-01-08 15:34:51 +0200

    Setup sorting problems

---
+++ src/sorting/NumberOfDiscIntersections.js
@@ -0,0 +1,5 @@
+// NumberOfDiscIntersections
+// Compute the number of intersections in a sequence of discs.
+// Score: 100%
+
+module.exports = function solution(A, B, K) {};
---
+++ test/sorting/NumberOfDiscIntersections.spec.js
@@ -0,0 +1,7 @@
+var test = require('tape');
+var numberOfDiscIntersections = require('../../src/sorting/NumberOfDiscIntersections');
+
+test('NumberOfDiscIntersections', function (t) {
+  console.log(numberOfDiscIntersections);
+  t.end();
+});
