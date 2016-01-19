6d9e24b5cfcf2350f5304d0698f005bc8516c67d
Tyrone Michael Avnit <tyronemichael@gmail.com>
2016-01-19 14:18:34 +0200

    Complete Sorted/Distinct

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-// ITERATIONS
-require('./01-iterations/BinaryGap');
-// ARRAYS
-require('./02-arrays/OddOccurrencesInArray');
-// TIME COMPLEXITY
-require('./03-time_complexity/TapeEquilibrium');
-require('./03-time_complexity/PermMissingElem');
-require('./03-time_complexity/FrogJmp');
-// COUNTING ELEMENTS
-require('./04-counting_elements/FrogRiverOne');
-require('./04-counting_elements/PermCheck');
-require('./04-counting_elements/MissingInteger');
-require('./04-counting_elements/MaxCounters');
-// PREFIX SUMS
-require('./05-prefix_sums/CountDiv');
-require('./05-prefix_sums/PassingCars');
-require('./05-prefix_sums/GenomicRangeQuery');
-require('./05-prefix_sums/MinAvgTwoSlice');
-//SORTING
-require('./06-sorting/NumberOfDiscIntersections');
-require('./06-sorting/MaxProductOfThree');
-require('./06-sorting/Triangle');
+// // ITERATIONS
+// require('./01-iterations/BinaryGap');
+// // ARRAYS
+// require('./02-arrays/OddOccurrencesInArray');
+// // TIME COMPLEXITY
+// require('./03-time_complexity/TapeEquilibrium');
+// require('./03-time_complexity/PermMissingElem');
+// require('./03-time_complexity/FrogJmp');
+// // COUNTING ELEMENTS
+// require('./04-counting_elements/FrogRiverOne');
+// require('./04-counting_elements/PermCheck');
+// require('./04-counting_elements/MissingInteger');
+// require('./04-counting_elements/MaxCounters');
+// // PREFIX SUMS
+// require('./05-prefix_sums/CountDiv');
+// require('./05-prefix_sums/PassingCars');
+// require('./05-prefix_sums/GenomicRangeQuery');
+// require('./05-prefix_sums/MinAvgTwoSlice');
+// //SORTING
+// require('./06-sorting/NumberOfDiscIntersections');
+// require('./06-sorting/MaxProductOfThree');
+// require('./06-sorting/Triangle');
+require('./06-sorting/Distinct');
